8090bb8b702988e57b69c9f5f58f40ed9b4efa0a
Pekka Maanpää <pekkamaa@vaadin.com>
2020-09-07 14:54:04 +0300

    Tweak about page contents

--- src/main/java/com/jensjansson/ce/views/about/AboutView.java
+++ src/main/java/com/jensjansson/ce/views/about/AboutView.java
@@ -18,17 +18,17 @@ public class AboutView extends FlexLayout {
 
         //@formatter:off
         String content = "<div>" +
-              "<h3>Demo Features</h3>" +
-              "<p>This application demonstrates the collaborative experiences that you can create with the Vaadin Collaboration Engine. " +
-                "Clicking a table row in the persons page opens a simple form for editing the information of that person. " +
+              "<h3>Demo features</h3>" +
+              "<p>This application demonstrates the collaborative experiences that you can create with Vaadin Collaboration Engine. " +
+                "Clicking a table row in the persons view opens a form for editing the information of that person. " +
                 "To enable real-time collaboration with other users, the form has these additional features:" +
                 "<ul>" +
-                  "<li>The avatars right above the form indicate all the other users who are looking at the same form at the same time. Your own avatar, which you have picked in the profile page, will be displayed to the other users here. Try hovering your mouse over the avatars to reveal the user names.</li>" +
-                  "<li>The fields indicate when other users are editing them by showing a highlight around the component.</li>" +
-                  "<li>When any users enters a new value to a field, the value is updated in the views of the other users who are editing the same person.</li>" +
+                  "<li>Avatars of other users will be displayed above the form if there are other users editing the same person. Your own avatar, which you have picked in the profile page, will be displayed to the other users. Try hovering your mouse over the avatars to reveal the users' names.</li>" +
+                  "<li>The fields indicate when other users are editing them by showing a highlight around the component. You can see who is editing by hovering over the indicator. The color of the highlight indicator matches the color of the avatar.</li>" +
+                  "<li>When any user enters a new value to a field, the value is displayed to other users in the same form.</li>" +
                 "</ul>" +
               "</p>" +
-              "<h3>How to Test the Collaboration</h3>" +
+              "<h3>How to test the collaboration</h3>" +
               "<p>" +
                 "To make it easy to see the collaborative features in action even if no one else is currently editing the same person as you are, " +
                 "we have implemented artificial bot users. If you start editing a person that no one else is currently editing, a bot user should join shortly. " +
@@ -38,7 +38,7 @@ public class AboutView extends FlexLayout {
                 "You can also test the collaboration by opening the app in two browser windows side by side. " +
                 "Open the same person editor in both windows, make changes to the fields in one window and notice how the other one is updated." +
               "</p>" +
-              "<h3>Where to Go Next</h3>" +
+              "<h3>Learn more</h3>" +
               "<p>" +
                 "To learn more about the Collaboration Engine, visit <a href=\"https://vaadin.com/collaboration\" target=\"_blank\">the Vaadin web site</a>." +
               "</p>" +
