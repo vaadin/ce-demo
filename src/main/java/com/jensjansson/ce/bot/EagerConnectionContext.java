fa1426df11d7f6bd549fcf1d9bb1bf70576bd11b
tanbui <trungtanbui@gmail.com>
2020-10-01 10:43:36 +0300

    Update with alpha13 using completable future

--- src/main/java/com/jensjansson/ce/bot/EagerConnectionContext.java
+++ src/main/java/com/jensjansson/ce/bot/EagerConnectionContext.java
@@ -1,5 +1,7 @@
 package com.jensjansson.ce.bot;
 
+import java.util.concurrent.CompletableFuture;
+
 import com.vaadin.collaborationengine.ActivationHandler;
 import com.vaadin.collaborationengine.ConnectionContext;
 import com.vaadin.flow.server.Command;
@@ -16,5 +18,10 @@ public class EagerConnectionContext implements ConnectionContext {
     @Override
     public void dispatchAction(Command command) {
         command.execute();
+    }
+
+    @Override
+    public <T> CompletableFuture<T> createCompletableFuture() {
+        return new CompletableFuture<>();
     }
 }
