05902b98816251f01acdb7ceb55a6753e6b7e763
Jens Jansson <peppe@vaadin.com>
2021-03-26 08:17:21 +0200

    register collaborationengineconfiguration

--- src/main/java/com/jensjansson/ce/Application.java
+++ src/main/java/com/jensjansson/ce/Application.java
@@ -1,12 +1,19 @@
 package com.jensjansson.ce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.vaadin.collaborationengine.CollaborationEngine;
+import com.vaadin.collaborationengine.CollaborationEngineConfiguration;
+import com.vaadin.collaborationengine.LicenseEventHandler;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
 import com.vaadin.flow.theme.Theme;
 
 /**
@@ -17,10 +24,35 @@ import com.vaadin.flow.theme.Theme;
 @Push
 @Theme("ce-demo")
 public class Application extends SpringBootServletInitializer
-        implements AppShellConfigurator {
+        implements AppShellConfigurator, VaadinServiceInitListener {
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void serviceInit(ServiceInitEvent serviceInitEvent) {
+        LicenseEventHandler licenseEventHandler = licenseEvent -> {
+            switch (licenseEvent.getType()) {
+            case GRACE_PERIOD_STARTED:
+            case LICENSE_EXPIRES_SOON:
+
+                LOGGER.warn(licenseEvent.getMessage());
+                break;
+            case GRACE_PERIOD_ENDED:
+            case LICENSE_EXPIRED:
+                LOGGER.error(licenseEvent.getMessage());
+                break;
+            default:
+                LOGGER.error("Unknown error: " + licenseEvent.getMessage());
+            }
+        };
+        CollaborationEngineConfiguration configuration = new CollaborationEngineConfiguration(
+                licenseEventHandler);
+        CollaborationEngine.configure(serviceInitEvent.getSource(),
+                configuration);
+    }
 }
